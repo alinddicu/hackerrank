f2185b73f307a776eb99c87e621307334d9ac427
alinddicu@yahoo.com <alinddicu@yahoo.com>
2016-05-22 15:48:58 +0200

    test environement

--- hackerrank/hackerrank/EskerTest.cs
+++ hackerrank/hackerrank/EskerTest.cs
@@ -1,9 +1,21 @@
 ﻿namespace hackerrank
 {
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using NFluent;
+
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class EskerTest
     {
+        [TestMethod]
+        public void CheckEnvironnemment()
+        {
+            var enumerable = string.Empty.Select(c => c.ToString());
+            // throw new NotImplementedException();
+            Check.That(true).IsTrue();
+        }
     }
 }
